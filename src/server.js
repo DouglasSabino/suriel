b4e1b5a93081534c320f41cda438adb0e7dcc1bd
Douglas Sabino <douglasbsabino@hotmail.com>
2025-07-24 23:01:22 -0300

    Arquiquetura basica e configuração do node/express

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const { httpstatuscode } = require('./util/httpstatuscode');
+
+const cors = require('cors');
+const express = require('express');
+require('dotenv').config();
+const { PORT } = process.env;
+
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+
+app.get('/', (_req, res) => {
+  res.status(httpstatuscode.OK).json("Tudo Certo :D");
+});
+
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
